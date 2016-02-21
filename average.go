a86f4917019fe891c5b153b9d2f70a9a8053395d
Chris Holmes <chris@holmser.net>
2016-02-21 13:24:01 -0800

    average

--- average.go
+++ average.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-  var x [5]float64
-  x[0] = 50
-  x[1] = 78
-  x[2] = 94
-  x[3] = 89
-  x[4] = 100
+  // var x [5]float64
+  // x[0] = 50
+  // x[1] = 78
+  // x[2] = 94
+  // x[3] = 89
+  // x[4] = 100
   //fmt.Println(x)
-
+  x := [5]float64{ 98, 93, 77, 82, 83 }
+  
   var total float64 = 0
   // for i := 0; i< len(x); i++ {
   //   total += x[i]
